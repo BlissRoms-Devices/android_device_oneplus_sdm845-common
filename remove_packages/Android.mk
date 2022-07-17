3c81c6b3166c7c9981ff4f994f95946ced25452a
srgrusso <srgrusso240@gmail.com>
2022-07-16 20:53:05 -0500

    sdm845-common: remove_packages: Cleanup

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,29 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Photos Chrome Chrome-Stub Camera2 CarrierSetup ConnMO DCMO DMService DevicePolicyPrebuilt DiagnosticsToolPrebuilt Drive HelpRtcPrebuilt Maps Photos PixelLiveWallpaperPrebuilt PixelWallpapers2021 PrebuiltGmail RecorderPrebuilt SafetyHubPrebuilt MicropaperPrebuilt CalculatorGooglePrebuilt CalendarGooglePrebuilt talkback
+LOCAL_OVERRIDES_PACKAGES := \
+	CalculatorGooglePrebuilt \
+	CalendarGooglePrebuilt \
+	Camera2 \
+	CarrierSetup \
+	Chrome \
+	Chrome-Stub \
+	ConnMO \
+	DCMO \
+	DevicePolicyPrebuilt \
+	DiagnosticsToolPrebuilt \
+	DMService \
+	Drive \
+	HelpRtcPrebuilt \
+	Maps \
+	MicropaperPrebuilt \
+	Photos \
+	PixelLiveWallpaperPrebuilt \
+	PixelWallpapers2021 \
+	PrebuiltGmail \
+	RecorderPrebuilt \
+	SafetyHubPrebuilt \
+	talkback
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
